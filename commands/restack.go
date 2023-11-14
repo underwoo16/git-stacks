94543f653df5370727ee24439c328bb467111a91
Chandler Underwood <underwoo16@up.edu>
2023-11-13 19:06:14 -0800

    Continued restack

--- commands/restack.go
+++ commands/restack.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"fmt"
 
+	"github.com/underwoo16/git-stacks/colors"
+	"github.com/underwoo16/git-stacks/git"
 	"github.com/underwoo16/git-stacks/stacks"
 )
 
@@ -10,18 +12,29 @@ func Restack() {
 	// TODO: use cache if exists
 	// TODO: get subtree starting from current branch
 	trunk := stacks.BuildStackGraphFromScratch()
-	restackChildren([]*stacks.StackNode{trunk}, trunk.RefSha)
+	restackChildren([]*stacks.StackNode{trunk}, trunk.ParentRefSha)
+
+	// TODO: is there a way to rebase other branches without the side effect of switching to them?
+	git.CheckoutBranch(trunk.Name)
 }
 
 func restackChildren(children []*stacks.StackNode, parentSha string) {
 	for _, child := range children {
-		if child.ParentRefSha != parentSha {
-			fmt.Printf("%s onto %s\n", child.Name, child.ParentBranch)
-		} else {
-			fmt.Printf("%s already up to date with %s\n", child.Name, child.ParentBranch)
+		if child.Parent == nil {
+			restackChildren(child.Children, child.RefSha)
+			continue
 		}
 
-		// TODO: update refSha if rebased
+		childName := colors.CurrentStack(child.Name)
+		parentName := colors.OtherStack(child.Parent.Name)
+		if child.ParentRefSha != parentSha {
+			// TODO: update child.refSha if rebased
+			fmt.Printf("%s restacking onto %s\n", childName, parentName)
+			git.Rebase(child.ParentBranch, child.Name)
+		} else {
+			fmt.Printf("%s up to date with %s\n", childName, parentName)
+		}
+
 		restackChildren(child.Children, child.RefSha)
 	}
 }
