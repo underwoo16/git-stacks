941869cc64d3b3f8c3678cbd99aaa69f403097b7
Chandler <underwoo16@up.edu>
2023-11-25 17:11:27 +0000

    Added initial test for metadata service using mocks

--- metadata/metadata_test.go
+++ metadata/metadata_test.go
@@ -4,8 +4,17 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/underwoo16/git-stacks/git"
+	"github.com/underwoo16/git-stacks/utils"
 )
 
 func TestConfigExists(t *testing.T) {
-	assert.True(t, 1 == 1, "1 should be equal to 1")
+	mockGitService := git.NewMockGitService(t)
+	mockFileService := utils.NewMockFileService(t)
+
+	mockGitService.On("DirectoryPath").Return("/tmp")
+	mockFileService.On("FileExists", "/tmp/.stacks_config").Return(true)
+
+	metadataService := NewMetadataService(mockGitService, mockFileService)
+	assert.True(t, metadataService.ConfigExists(), "ConfigExists should return true")
 }
